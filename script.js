7f85bf292a3a13ed9b1d429b79cfeb9ae2b57f47
hamzajaradat666 <akumasoul666@gmail.com>
2018-06-25 18:47:13 +0300

    v0.0.1.1

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let ctx2 = canvas.getContext("2d");
 let canvasW = document.body.clientWidth;
 let canvasH = document.body.clientHeight / 3 * 2;
 let translate_rate = 8;
+let generate = false;
 
 let key = {
 
@@ -30,6 +31,22 @@ let board_height = 1;
 let closeX = 1.53;
 let closeY = 1.76;
 
+var levelData=[
+[-1,-1,-1,0,0,0,0,0,0,0,-1,-1,-1],
+[-1,-1,0,0,0,0,0,0,0,0,-1,-1,-1],
+[-1,-1,0,0,0,0,0,0,0,0,0,-1,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,-1,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,-1],
+[0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,0,-1],
+[-1,0,0,0,0,0,0,0,0,0,0,-1,-1],
+[-1,-1,0,0,0,0,0,0,0,0,0,-1,-1],
+[-1,-1,0,0,0,0,0,0,0,0,-1,-1,-1],
+[-1,-1,-1,0,0,0,0,0,0,0,-1,-1,-1]
+];
+
 
 class Mapper {
 
@@ -55,9 +72,9 @@ class Mapper {
             cellnum: 1,
             type: {
 
-                disabled:1,
-                land:2,
-                water:3
+                disabled: 1,
+                land: 2,
+                water: 3
 
             },
             isOn: false
@@ -90,6 +107,7 @@ class Mapper {
                 this.hex.y = i % 2 == 0 ? this.hex.y * j * this.cY + this.hex.in_r * 2 : this.hex.y * j * this.cY + this.hex.in_r;
                 if (map.length <= this.height * this.width) {
 
+                    
                     if (map.length <= this.width * this.height - 1)
                         map.push({ ...this.hex
                         });
@@ -114,45 +132,36 @@ class Mapper {
 
         this.props();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
 
-            var map = [];
-            var rows = board_width;
-            var columns = board_height;
-            for (var i = 0; i < rows; i++) {
-                map.push([0])
-                for (var j = 0; j < columns; j++) {
-                    map[i][j] = 0;
-                }
-            }
-        
+        let map = []
+        let inner_map = [];
+      
+        for (let i = this.initPosY; i < this.width + this.initPosY; i += 1) {
 
-        for (let j = this.initPosX; j < this.height + this.initPosX; j += 1) {
-            for (let i = this.initPosY; i < this.width + this.initPosY; i += 1) {
-                this.hex.x = this.hex.x * i * this.cX;
-                this.hex.y = i % 2 == 0 ? this.hex.y * j * this.cY + this.hex.in_r * 2 : this.hex.y * j * this.cY + this.hex.in_r;
-                if (map.length <= this.height * this.width) {
+            for (let j = this.initPosX; j < this.height + this.initPosX; j += 1) {
 
-                    if (map.length <= this.width * this.height - 1)
-                        map.push([{ ...this.hex}]);
+                this.hex.x = this.hex.x * j * this.cX;
+                this.hex.y = j % 2 == 0 ? this.hex.y * i * this.cY + this.hex.in_r * 2 : this.hex.y * i * this.cY + this.hex.in_r;
 
-                    // console.log(this.hex)
-
-                }
-
-
-
+                  
+                inner_map.push({ ...this.hex});
+                   
+                    
+                        
                 this.hex.x = this.cellside;
                 this.hex.y = this.cellside;
                 this.hex.cellnum++;
             }
+            map.push(inner_map);
+            inner_map=[];
+
         }
 
         this.hex.cellnum = 1;
         return map;
     }
 
-    mapGenerator2D(mapC){
+    mapGenerator2D(mapC) {
 
 
 
@@ -160,6 +169,7 @@ class Mapper {
     }
 
     mapDrawer(mapC) {
+        ctx.clearRect(0, 0, canvasW, canvasH);
         for (let i = 0; i < mapC.length; i++) {
             ctx.save();
             ctx.strokeStyle = "rgba(100, 0, 50, 1)";
@@ -180,24 +190,25 @@ class Mapper {
 
     }
 
-    mapDrawer2D(mapC) {
-        for (let i = 0; i < mapC.length; i++) 
-        for (let j=0;j<mapC[i].length;j++){
-            ctx.save();
-            ctx.strokeStyle = "rgba(100, 0, 50, 1)";
-            ctx.beginPath();
-            ctx.moveTo(mapC[i][j].x, mapC[i][j].y);
-            ctx.moveTo(mapC[i][j].x - mapC[i][j].cir_R, mapC[i][j].y)
-            ctx.lineTo(mapC[i][j].x - mapC[i][j].side / 2, mapC[i][j].y - mapC[i][j].in_r)
-            ctx.lineTo(mapC[i][j].x + mapC[i][j].side / 2, mapC[i][j].y - mapC[i][j].in_r)
-            ctx.lineTo(mapC[i][j].x + mapC[i][j].cir_R, mapC[i][j].y);
-            ctx.lineTo(mapC[i][j].x + mapC[i][j].side / 2, mapC[i][j].y + mapC[i][j].in_r)
-            ctx.lineTo(mapC[i][j].x - mapC[i][j].side / 2, mapC[i][j].y + mapC[i][j].in_r)
-            ctx.fillText(mapC[i][j].cellnum, mapC[i][j].x, mapC[i][j].y);
-            ctx.closePath();
-            ctx.stroke();
-            ctx.restore();
-        }
+    mapDrawer2D_vertical(mapC) {
+        ctx.clearRect(0, 0, canvasW, canvasH);
+        for (let i = 0; i < mapC.length; i++)
+            for (let j = 0; j < mapC[i].length; j++) {
+                ctx.save();
+                ctx.strokeStyle = "rgba(100, 0, 50, 1)";
+                ctx.beginPath();
+                ctx.moveTo(mapC[i][j].x, mapC[i][j].y);
+                ctx.moveTo(mapC[i][j].x - mapC[i][j].cir_R, mapC[i][j].y)
+                ctx.lineTo(mapC[i][j].x - mapC[i][j].side / 2, mapC[i][j].y - mapC[i][j].in_r)
+                ctx.lineTo(mapC[i][j].x + mapC[i][j].side / 2, mapC[i][j].y - mapC[i][j].in_r)
+                ctx.lineTo(mapC[i][j].x + mapC[i][j].cir_R, mapC[i][j].y);
+                ctx.lineTo(mapC[i][j].x + mapC[i][j].side / 2, mapC[i][j].y + mapC[i][j].in_r)
+                ctx.lineTo(mapC[i][j].x - mapC[i][j].side / 2, mapC[i][j].y + mapC[i][j].in_r)
+                ctx.fillText(/* mapC[i][j].cellnum+ "-"+*/i+"-"+j, mapC[i][j].x, mapC[i][j].y);
+                ctx.closePath();
+                ctx.stroke();
+                ctx.restore();
+            }
 
 
     }
@@ -227,197 +238,226 @@ class Mapper {
                 }
             }
 
-                if (mapC[i].isOn == true || hexhover(mapC[i])) {
+            if (mapC[i].isOn == true || hexhover(mapC[i])) {
 
-                    this.fillHex(mapC[i], c);
-                }
-
-
-
+                this.fillHex(mapC[i], c);
             }
-            md = false;
+
+
+
         }
+        md = false;
+    }
 
-        mapChecker2D(mapC) {
-            let c = {
-                r: 133,
-                g: 23,
-                b: 1,
-                a: 0.55
-            }
-            for (let i = 0; i < mapC.length; i++) 
-            for (let j=0;j<mapC[i].length;j++){
-    
+    mapChecker2D(mapC) {
+        let c = {
+            r: 133,
+            g: 213,
+            b: 1,
+            a: 0.55
+        }
+        for (let i = 0; i < mapC.length; i++)
+            for (let j = 0; j < mapC[i].length; j++) {
+
                 if (hexhover(mapC[i][j]) && hexClick(mapC[i][j]) && mapC[i][j].isOn == false) {
-    
+
                     mapC[i][j].isOn = true;
                     this.fillHex(mapC[i][j], c)
-    
+
                 } else if (hexhover(mapC[i][j]) && mapC[i][j].isOn == true) {
-    
+
                     this.fillHex(mapC[i][j], c)
-    
+
                     if (hexClick(mapC[i][j])) {
-    
+
                         mapC[i][j].isOn = false;
                         this.fillHex(mapC[i][j], c)
                     }
                 }
-    
-                    if (mapC[i][j].isOn == true || hexhover(mapC[i][j])) {
-    
-                        this.fillHex(mapC[i][j], c);
-                    }
-    
-    
-    
+
+                if (mapC[i][j].isOn == true || hexhover(mapC[i][j])) {
+
+
+                    this.fillHex(mapC[i][j], c);
+
+
+
                 }
-                md = false;
+
+                if (mapC[i][j].isOn == true && hexhover(mapC[i][j])) {
+
+                    
+                    this.fillHex(mapC[i+1][j], c);
+                    this.fillHex(mapC[i+2][j], c);
+                    this.fillHex(mapC[i+3][j], c);
+                    this.fillHex(mapC[i+4][j], c);
+                    this.fillHex(mapC[i+5][j], c);
+                    this.fillHex(mapC[i+6][j], c);
+                    this.fillHex(mapC[i+7][j], c);
+                    this.fillHex(mapC[i+8][j], c);
+                    this.fillHex(mapC[i+9][j], c);
+                    this.fillHex(mapC[i][j], c);
+
+                }
+
+
+
             }
+        md = false;
+    }
 
-        fillHex(cell, c) {
-            let color = "rgba(" + c.r + "," + c.g + ", " + c.b + ", " + c.a + ")";
-            ctx.save();
-            // ctx.strokeStyle = "rgba(200, 10, 100, 1)";
-            ctx.strokeStyle = color;
-            ctx.beginPath();
-            ctx.moveTo(cell.x, cell.y);
-            ctx.moveTo(cell.x - cell.cir_R, cell.y)
-            ctx.lineTo(cell.x - cell.side / 2, cell.y - cell.in_r)
-            ctx.lineTo(cell.x + cell.side / 2, cell.y - cell.in_r)
-            ctx.lineTo(cell.x + cell.cir_R, cell.y);
-            ctx.lineTo(cell.x + cell.side / 2, cell.y + cell.in_r)
-            ctx.lineTo(cell.x - cell.side / 2, cell.y + cell.in_r)
-            ctx.closePath();
-            ctx.fillStyle = color;
-            ctx.fill();
-            ctx.restore();
-
-
-        }
-
-
-        props() {
-
-
-            this.cellside = cellside;
-            this.width = Math.floor(parseInt(document.getElementById("borderwidth").value) / 2) * 2;
-            this.height = parseInt(document.getElementById("borderheight").value);
-            this.initPosX = Math.floor(parseInt(document.getElementById("initposX").value) / 2) * 2;
-            this.initPosY = Math.floor(parseInt(document.getElementById("initposY").value) / 2) * 2;
-
-
-
-
-        }
+    fillHex(cell, c) {
+        let color = "rgba(" + c.r + "," + c.g + ", " + c.b + ", " + c.a + ")";
+        ctx.save();
+        // ctx.strokeStyle = "rgba(200, 10, 100, 1)";
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(cell.x, cell.y);
+        ctx.moveTo(cell.x - cell.cir_R, cell.y)
+        ctx.lineTo(cell.x - cell.side / 2, cell.y - cell.in_r)
+        ctx.lineTo(cell.x + cell.side / 2, cell.y - cell.in_r)
+        ctx.lineTo(cell.x + cell.cir_R, cell.y);
+        ctx.lineTo(cell.x + cell.side / 2, cell.y + cell.in_r)
+        ctx.lineTo(cell.x - cell.side / 2, cell.y + cell.in_r)
+        ctx.closePath();
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.restore();
 
 
     }
 
 
-    class Unit {
-
-        constructor() {
-
-            console.log(" Unit Constructer");
-            this.range = 20;
-            this.pieces = [1];
-            this.basePiece = 99;
-            this.attack = 20;
-            this.defence = 0;
-            this.hp = 0;
-            this.isSelected = false;
-            this.moveable = false;
-            this.isPassable = false;
+    props() {
 
 
+        this.cellside = cellside;
+        this.width = Math.floor(parseInt(document.getElementById("borderwidth").value) / 2) * 2;
+        this.height = parseInt(document.getElementById("borderheight").value);
+        this.initPosX = Math.floor(parseInt(document.getElementById("initposX").value) / 2) * 2;
+        this.initPosY = Math.floor(parseInt(document.getElementById("initposY").value) / 2) * 2;
 
-        }
+
 
 
     }
 
-    class Soilder extends Unit {
 
-        constructor() {
+}
 
-            super();
 
-        }
+class Unit {
+
+    constructor() {
+
+        console.log(" Unit Constructer");
+        this.range = 20;
+        this.pieces = [1];
+        this.basePiece = 99;
+        this.attack = 20;
+        this.defence = 0;
+        this.hp = 0;
+        this.isSelected = false;
+        this.moveable = false;
+        this.isPassable = false;
+
+
 
     }
 
-    class Tank extends Unit {
 
-        constructor() {
+}
 
-            super();
+class Soilder extends Unit {
+
+    constructor() {
+
+        super();
+
+    }
+
+}
+
+class Tank extends Unit {
+
+    constructor() {
+
+        super();
 
 
-        }
+    }
+}
+
+
+
+
+
+
+function main_menu() {
+
+
+
+    if (translate_rate > mx) {
+        ctx.translate(translate_rate, 0)
+        mx += translate_rate;
+    }
+
+    if (mx > canvasW - translate_rate) {
+        ctx.translate(-translate_rate, 0)
+        mx -= translate_rate;
+    }
+
+    if (translate_rate > my) {
+        ctx.translate(0, translate_rate)
+        my += translate_rate;
+    }
+
+    if (my > canvasH - translate_rate) {
+        ctx.translate(0, -translate_rate)
+        my -= translate_rate;
     }
 
 
 
+}
+let map0 = new Mapper();
+let main_map;
 
-    let map0 = new Mapper();
-    let main_map = map0.make2D();
+function the_props(){
+
+    
+    map0.props();
+    main_map = map0.make2D();
     console.log(main_map)
 
-    function main_menu() {
+}
+
+function update() {
+
+    
+    map0.mapDrawer2D_vertical(main_map);
+    map0.mapChecker2D(main_map);
 
 
+}
 
-        if (translate_rate > mx) {
-            ctx.translate(translate_rate, 0)
-            mx += translate_rate;
-        }
-
-        if (mx > canvasW - translate_rate) {
-            ctx.translate(-translate_rate, 0)
-            mx -= translate_rate;
-        }
-
-        if (translate_rate > my) {
-            ctx.translate(0, translate_rate)
-            my += translate_rate;
-        }
-
-        if (my > canvasH - translate_rate) {
-            ctx.translate(0, -translate_rate)
-            my -= translate_rate;
-        }
-
-
-
+function timer() {
+    fps++
+    if (fps > 30) {
+        time++;
+        fps = 0;
     }
+    if (time > 3600)
+        time = 0;
 
-    function update() {
+}
+window.addEventListener("load", function () {
 
-        ctx.clearRect(0, 0, canvasW, canvasH);
-        map0.mapDrawer2D(main_map);
-        map0.mapChecker2D(main_map);
+    document.getElementById("testarea").innerHTML = "Welcome To Rangers";
+    document.getElementById("drawarea").setAttribute("width", canvasW);
+    document.getElementById("drawarea").setAttribute("height", canvasH);
 
-
-    }
-
-    function timer() {
-        fps++
-        if (fps > 30) {
-            time++;
-            fps = 0;
-        }
-        if (time > 3600)
-            time = 0;
-
-    }
-    window.addEventListener("load", function () {
-
-        document.getElementById("testarea").innerHTML = "Welcome To Rangers";
-        document.getElementById("drawarea").setAttribute("width", canvasW);
-        document.getElementById("drawarea").setAttribute("height", canvasH);
-
-        setInterval(update, 1000 / fps);
+    setInterval(update, 1000 / fps);
 
 
 
@@ -425,4 +465,4 @@ class Mapper {
 
 
 
-    })
+})
